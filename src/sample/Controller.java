6be417ce9789ea96f6f3b5c7e938b98d75d20d0c
Max Villeneuve <46503113+maxvill212@users.noreply.github.com>
2019-03-11 17:56:09 -0400

    This is broken, forget it
    I tried renaming stuff to something other then "Sample" and broke everthing. I also deleted the project so nothing is here

--- src/sample/Controller.java
+++
@@ -1,9 +0,0 @@
-package sample;
-
-import javafx.event.ActionEvent;
-
-public class Controller {
-    public void ButtonPress(ActionEvent event){
-        System.out.println("Hello World");
-    }
-}
