2fb39d414f7cb7442b780eae7ebd00c72e2e4c96
Max Villeneuve <46503113+maxvill212@users.noreply.github.com>
2019-03-10 22:36:31 -0400

    Basic button that outputs in terminal

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,9 @@
 package sample;
 
+import javafx.event.ActionEvent;
+
 public class Controller {
+    public void ButtonPress(ActionEvent event){
+        System.out.println("Hello World");
+    }
 }
