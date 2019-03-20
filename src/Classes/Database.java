53ac4ec6c925fc4cffcae575419278f7786001d9
Max Villeneuve <46503113+maxvill212@users.noreply.github.com>
2019-03-19 21:07:42 -0400

    Connecting to a database successfully

--- src/Classes/Database.java
+++ src/Classes/Database.java
@@ -1,4 +1,31 @@
 package Classes;
 
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.DriverManager;
+
 public class Database {
+
+    private Connection conn = null;
+    private Statement stm = null;
+
+    public void connect(String user, String pass){
+        try{
+            String url = "jdbc:sqlite:src/Database/SQLite_database.db";
+            conn = DriverManager.getConnection(url);
+            System.out.println("Connection to database successful");
+        } catch (SQLException e){
+            System.out.println(e.getMessage());
+        } finally {
+            try{
+                if (conn != null){
+                    conn.close();
+                }
+            }catch (SQLException ex){
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+//    String sql = "CREATE TABLE IF NOT EXIST `user`";
 }
--- src/Controller/WelcomeWindow.java
+++ src/Controller/WelcomeWindow.java
@@ -1,16 +1,30 @@
 package Controller;
 
+import Classes.Database;
+import Classes.ValidateLogin;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+
+import java.util.Hashtable;
 
 public class WelcomeWindow {
 
     @FXML
     private Button btnStart;
+    @FXML
+    private TextField txtUsername;
+    @FXML
+    private TextField txtPass;
+
+    String user, password;
+
+    Database database = new Database();
 
 
     public void start() throws Exception{
@@ -24,7 +38,17 @@ public class WelcomeWindow {
        primaryStage.show();
     }
 
+    public void login(ActionEvent e) throws Exception{
+        user = txtUsername.getText();
+        password = txtPass.getText();
 
+        database.connect(user, password);
+
+
+//        ValidateLogin validate = new ValidateLogin();
+
+//        Hashtable<String, String> userPass = new Hashtable<>();
+    }
 
 
 }
