bdf247be22082d145c9ee335b3aef8166be31834
Max Villeneuve <46503113+maxvill212@users.noreply.github.com>
2019-04-04 23:37:57 -0400

    Trying to figure out why the for loop in calc isnt working

--- src/Classes/Calculate.java
+++ src/Classes/Calculate.java
@@ -48,134 +48,139 @@ public class Calculate {
     }
 
 
-//    public int average(){
-//
-//        result = pull();
-//        numValue(result);
-//
-//
-//        return avg;
-//    }
-
 //    Method that converts all answers to integer values
-    public double[][] numValue(String [][] results){
+    public double[][] numValue(String[][] results){
         for (int i = 0; i < rsSize; i++) {
 
-            if (results[i][2] == "1"){
+            if (results[i][1] == "1"){
+                numValue[i][1] = 1;
+            }else if (results[i][1] == "2"){
+                numValue[i][1] = 2;
+            }else if (results[i][1] == "3"){
+                numValue[i][1] = 3;
+            }else if (results[i][1] == "4"){
+                numValue[i][1] = 4;
+            }else if (results[i][1] == "5"){
+                numValue[i][1] = 5;
+            }else if (results[i][1] == "6"){
+                numValue[i][1] = 6;
+            }else if (results[i][1] == "7+"){
+                numValue[i][1] = 7;
+            }
+
+            if (results[i][2] == "Apartment"){
                 numValue[i][2] = 1;
-            }else if (results[i][2] == "2"){
+            }else if (results[i][2] == "Semi Detached"){
                 numValue[i][2] = 2;
-            }else if (results[i][2] == "3"){
+            }else if (results[i][2] == "Town House"){
                 numValue[i][2] = 3;
-            }else if (results[i][2] == "4"){
+            }else if (results[i][2] == "Standalone"){
                 numValue[i][2] = 4;
-            }else if (results[i][2] == "5"){
-                numValue[i][2] = 5;
-            }else if (results[i][2] == "6"){
-                numValue[i][2] = 6;
-            }else if (results[i][2] == "7+"){
-                numValue[i][2] = 7;
             }
 
-            if (results[i][3] == "Apartment"){
+            if (results[i][3] == "Bus"){
                 numValue[i][3] = 1;
-            }else if (results[i][3] == "Semi Detached"){
+            }else if (results[i][3] == "Gas/Diesel Car"){
                 numValue[i][3] = 2;
-            }else if (results[i][3] == "Town House"){
+            }else if (results[i][3] == "Bike"){
                 numValue[i][3] = 3;
-            }else if (results[i][3] == "Standalone"){
+            }else if (results[i][3] == "Walk"){
                 numValue[i][3] = 4;
+            }else if (results[i][3] == "Moped"){
+                numValue[i][3] = 5;
+            }else if (results[i][3] == "Electric Car/Hybrid"){
+                numValue[i][3] = 6;
             }
 
-            if (results[i][4] == "Bus"){
+            if (results[i][4] == "0-2"){
                 numValue[i][4] = 1;
-            }else if (results[i][4] == "Gas/Diesel Car"){
+            }else if (results[i][4] == "3-5"){
                 numValue[i][4] = 2;
-            }else if (results[i][4] == "Bike"){
+            }else if (results[i][4] == "6-10"){
                 numValue[i][4] = 3;
-            }else if (results[i][4] == "Walk"){
+            }else if (results[i][4] == "11+"){
                 numValue[i][4] = 4;
-            }else if (results[i][4] == "Moped"){
-                numValue[i][4] = 5;
-            }else if (results[i][4] == "Electric Car/Hybrid"){
-                numValue[i][4] = 6;
             }
 
-            if (results[i][5] == "0-2"){
+            if (results[i][5] == "Never"){
                 numValue[i][5] = 1;
-            }else if (results[i][5] == "3-5"){
+            }else if (results[i][5] == "1-2"){
                 numValue[i][5] = 2;
-            }else if (results[i][5] == "6-10"){
+            }else if (results[i][5] == "3-5"){
                 numValue[i][5] = 3;
-            }else if (results[i][5] == "11+"){
+            }else if (results[i][5] == "6-7"){
                 numValue[i][5] = 4;
             }
 
-            if (results[i][6] == "Never"){
+            if (results[i][6] == "Vegan"){
                 numValue[i][6] = 1;
-            }else if (results[i][6] == "1-2"){
+            }else if (results[i][6] == "Vegetarian"){
                 numValue[i][6] = 2;
-            }else if (results[i][6] == "3-5"){
+            }else if (results[i][6] == "No Meat"){
                 numValue[i][6] = 3;
-            }else if (results[i][6] == "6-7"){
+            }else if (results[i][6] == "Rarely Use Meat"){
                 numValue[i][6] = 4;
+            }else if (results[i][6] == "Meat in Some Meals"){
+                numValue[i][6] = 5;
+            }else if (results[i][6] == "Meat in Every Meal"){
+                numValue[i][6] = 6;
             }
 
-            if (results[i][7] == "Vegan"){
+            if (results[i][7] == "0"){
                 numValue[i][7] = 1;
-            }else if (results[i][7] == "Vegetarian"){
+            }else if (results[i][7] == "1-10"){
                 numValue[i][7] = 2;
-            }else if (results[i][7] == "No Meat"){
+            }else if (results[i][7] == "11-20"){
                 numValue[i][7] = 3;
-            }else if (results[i][7] == "Rarely Use Meat"){
+            }else if (results[i][7] == "21-50"){
                 numValue[i][7] = 4;
-            }else if (results[i][7] == "Meat in Some Meals"){
+            }else if (results[i][7] == "51+"){
                 numValue[i][7] = 5;
-            }else if (results[i][7] == "Meat in Every Meal"){
-                numValue[i][7] = 6;
             }
 
             if (results[i][8] == "0"){
                 numValue[i][8] = 1;
             }else if (results[i][8] == "1-10"){
                 numValue[i][8] = 2;
-            }else if (results[i][8] == "11-20"){
+            }else if (results[i][8] == "11-30"){
                 numValue[i][8] = 3;
-            }else if (results[i][8] == "21-50"){
+            }else if (results[i][8] == "31+"){
                 numValue[i][8] = 4;
-            }else if (results[i][8] == "51+"){
-                numValue[i][8] = 5;
             }
 
-            if (results[i][9] == "0"){
+            if (results[i][9] == "Hardly/At All"){
                 numValue[i][9] = 1;
-            }else if (results[i][9] == "1-10"){
+            }else if (results[i][9] == "Sometimes"){
                 numValue[i][9] = 2;
-            }else if (results[i][9] == "11-30"){
+            }else if (results[i][9] == "Very Often"){
                 numValue[i][9] = 3;
-            }else if (results[i][9] == "31+"){
-                numValue[i][9] = 4;
             }
 
-            if (results[i][10] == "Hardly/At All"){
+            if (results[i][10] == "Electric"){
                 numValue[i][10] = 1;
-            }else if (results[i][10] == "Sometimes"){
+            }else if (results[i][10] == "Plug-In Hybrid"){
                 numValue[i][10] = 2;
-            }else if (results[i][10] == "Very Often"){
+            }else if (results[i][10] == "Engine Charging Hybrid"){
                 numValue[i][10] = 3;
+            }else if (results[i][10] == "Small Diesel/Petrol"){
+                numValue[i][10] = 4;
+            }else if (results[i][10] == "Medium Diesel/Petrol"){
+                numValue[i][10] = 5;
+            }else if (results[i][10] == "Large Diesel/Petrol"){
+                numValue[i][10] = 6;
             }
 
-            if (results[i][11] == "Electric"){
+            if (results[i][11] == "0"){
                 numValue[i][11] = 1;
-            }else if (results[i][11] == "Plug-In Hybrid"){
+            }else if (results[i][11] == "1-5"){
                 numValue[i][11] = 2;
-            }else if (results[i][11] == "Engine Charging Hybrid"){
+            }else if (results[i][11] == "6-10"){
                 numValue[i][11] = 3;
-            }else if (results[i][11] == "Small Diesel/Petrol"){
+            }else if (results[i][11] == "11-15"){
                 numValue[i][11] = 4;
-            }else if (results[i][11] == "Medium Diesel/Petrol"){
+            }else if (results[i][11] == "16-20"){
                 numValue[i][11] = 5;
-            }else if (results[i][11] == "Large Diesel/Petrol"){
+            }else if (results[i][11] == "21-24"){
                 numValue[i][11] = 6;
             }
 
@@ -195,167 +200,137 @@ public class Calculate {
 
             if (results[i][13] == "0"){
                 numValue[i][13] = 1;
-            }else if (results[i][13] == "1-5"){
+            }else if (results[i][13] == "1-2"){
                 numValue[i][13] = 2;
-            }else if (results[i][13] == "6-10"){
+            }else if (results[i][13] == "3-5"){
                 numValue[i][13] = 3;
-            }else if (results[i][13] == "11-15"){
+            }else if (results[i][13] == "6-10"){
                 numValue[i][13] = 4;
-            }else if (results[i][13] == "16-20"){
+            }else if (results[i][13] == "11+"){
                 numValue[i][13] = 5;
-            }else if (results[i][13] == "21-24"){
-                numValue[i][13] = 6;
             }
 
-            if (results[i][14] == "0"){
+            if (results[i][14] == "1"){
                 numValue[i][14] = 1;
-            }else if (results[i][14] == "1-2"){
+            }else if (results[i][14] == "2"){
                 numValue[i][14] = 2;
-            }else if (results[i][14] == "3-5"){
+            }else if (results[i][14] == "3"){
                 numValue[i][14] = 3;
-            }else if (results[i][14] == "6-10"){
+            }else if (results[i][14] == "4"){
                 numValue[i][14] = 4;
-            }else if (results[i][14] == "11+"){
+            }else if (results[i][14] == "5+"){
                 numValue[i][14] = 5;
             }
 
-            if (results[i][15] == "1"){
+            if (results[i][15] == "Wood"){
                 numValue[i][15] = 1;
-            }else if (results[i][15] == "2"){
+            }else if (results[i][15] == "Electricity"){
                 numValue[i][15] = 2;
-            }else if (results[i][15] == "3"){
+            }else if (results[i][15] == "Natural Gas"){
                 numValue[i][15] = 3;
-            }else if (results[i][15] == "4"){
+            }else if (results[i][15] == "Oil"){
                 numValue[i][15] = 4;
-            }else if (results[i][15] == "5+"){
-                numValue[i][15] = 5;
             }
 
-            if (results[i][16] == "Wood"){
-                numValue[i][16] = 1;
-            }else if (results[i][16] == "Electricity"){
+            if (results[i][16] == "Don't Know"){
                 numValue[i][16] = 2;
-            }else if (results[i][16] == "Natural Gas"){
+            }else if (results[i][16] == "No"){
+                numValue[i][16] = 1;
+            }else if (results[i][16] == "Yes but not 100% Renewable"){
                 numValue[i][16] = 3;
-            }else if (results[i][16] == "Oil"){
+            }else if (results[i][16] == "Yes"){
                 numValue[i][16] = 4;
             }
 
-            if (results[i][17] == "Don't Know"){
-                numValue[i][17] = 2;
-            }else if (results[i][17] == "No"){
+            if (results[i][17] == "No"){
                 numValue[i][17] = 1;
-            }else if (results[i][17] == "Yes but not 100% Renewable"){
-                numValue[i][17] = 3;
             }else if (results[i][17] == "Yes"){
-                numValue[i][17] = 4;
+                numValue[i][17] = 2;
             }
 
-            if (results[i][18] == "No"){
-                numValue[i][18] = 1;
-            }else if (results[i][18] == "Yes"){
-                numValue[i][18] = 2;
-            }
-
-            if (results[i][19] == "Bellow 14"){
-                numValue[i][19] = 1;
-            }else if (results[i][19] == "14-17"){
-                numValue[i][19] = 1;
-            }else if (results[i][19] == "18-21"){
-                numValue[i][19] = 3;
-            }else if (results[i][19] == "Over 21"){
-                numValue[i][19] = 4;
-            }
-
-            if (results[i][20] == "0"){
+            if (results[i][20] == "Bellow 14"){
                 numValue[i][20] = 1;
-            }else if (results[i][20] == "1"){
-                numValue[i][20] = 2;
-            }else if (results[i][20] == "2"){
+            }else if (results[i][20] == "14-17"){
+                numValue[i][20] = 1;
+            }else if (results[i][20] == "18-21"){
                 numValue[i][20] = 3;
-            }else if (results[i][20] == "3"){
+            }else if (results[i][20] == "Over 21"){
                 numValue[i][20] = 4;
-            }else if (results[i][20] == "4"){
-                numValue[i][20] = 5;
-            }else if (results[i][20] == "5"){
-                numValue[i][20] = 6;
-            }else if (results[i][20] == "6"){
-                numValue[i][20] = 7;
-            }else if (results[i][20] == "7"){
-                numValue[i][20] = 8;
-            }else if (results[i][20] == "8"){
-                numValue[i][20] = 9;
             }
 
-            if (results[i][21] == "0-20"){
+            if (results[i][21] == "0"){
                 numValue[i][21] = 1;
-            }else if (results[i][21] == "21-50"){
+            }else if (results[i][21] == "1"){
                 numValue[i][21] = 2;
-            }else if (results[i][21] == "51-90"){
+            }else if (results[i][21] == "2"){
                 numValue[i][21] = 3;
-            }else if (results[i][21] == "91-150"){
+            }else if (results[i][21] == "3"){
                 numValue[i][21] = 4;
-            }else if (results[i][21] == "151-200"){
+            }else if (results[i][21] == "4"){
                 numValue[i][21] = 5;
-            }else if (results[i][21] == "201+"){
+            }else if (results[i][21] == "5"){
                 numValue[i][21] = 6;
+            }else if (results[i][21] == "6"){
+                numValue[i][21] = 7;
+            }else if (results[i][21] == "7"){
+                numValue[i][21] = 8;
+            }else if (results[i][21] == "8"){
+                numValue[i][21] = 9;
             }
 
-            if (results[i][22] == "0-50"){
+            if (results[i][22] == "0-20"){
                 numValue[i][22] = 1;
-            }else if (results[i][22] == "51-100"){
+            }else if (results[i][22] == "21-50"){
                 numValue[i][22] = 2;
-            }else if (results[i][22] == "101-150"){
+            }else if (results[i][22] == "51-90"){
                 numValue[i][22] = 3;
-            }else if (results[i][22] == "151-250"){
+            }else if (results[i][22] == "91-150"){
                 numValue[i][22] = 4;
-            }else if (results[i][22] == "251-300"){
+            }else if (results[i][22] == "151-200"){
                 numValue[i][22] = 5;
-            }else if (results[i][22] == "301+"){
+            }else if (results[i][22] == "201+"){
                 numValue[i][22] = 6;
             }
 
-            if (results[i][23] == "0-20"){
+            if (results[i][23] == "0-50"){
                 numValue[i][23] = 1;
-            }else if (results[i][23] == "21-40"){
+            }else if (results[i][23] == "51-100"){
                 numValue[i][23] = 2;
-            }else if (results[i][23] == "41-70"){
+            }else if (results[i][23] == "101-150"){
                 numValue[i][23] = 3;
-            }else if (results[i][23] == "71-100"){
+            }else if (results[i][23] == "151-250"){
                 numValue[i][23] = 4;
-            }else if (results[i][23] == "100+"){
+            }else if (results[i][23] == "251-300"){
                 numValue[i][23] = 5;
+            }else if (results[i][23] == "301+"){
+                numValue[i][23] = 6;
             }
 
-            if (results[i][24] == "0-50"){
+            if (results[i][24] == "0-20"){
                 numValue[i][24] = 1;
-            }else if (results[i][24] == "51-100"){
+            }else if (results[i][24] == "21-40"){
                 numValue[i][24] = 2;
-            }else if (results[i][24] == "101-150"){
+            }else if (results[i][24] == "41-70"){
                 numValue[i][24] = 3;
-            }else if (results[i][24] == "151-200"){
+            }else if (results[i][24] == "71-100"){
                 numValue[i][24] = 4;
-            }else if (results[i][24] == "201-250"){
+            }else if (results[i][24] == "100+"){
                 numValue[i][24] = 5;
-            }else if (results[i][24] == "251-300"){
-                numValue[i][24] = 6;
-            }else if (results[i][24] == "301+"){
-                numValue[i][24] = 7;
             }
 
-            if (results[i][25] == "0"){
+            if (results[i][25] == "0-50"){
                 numValue[i][25] = 1;
-            }else if (results[i][25] == "1"){
+            }else if (results[i][25] == "51-100"){
                 numValue[i][25] = 2;
-            }else if (results[i][25] == "2"){
+            }else if (results[i][25] == "101-150"){
                 numValue[i][25] = 3;
-            }else if (results[i][25] == "3"){
+            }else if (results[i][25] == "151-200"){
                 numValue[i][25] = 4;
-            }else if (results[i][25] == "4"){
+            }else if (results[i][25] == "201-250"){
                 numValue[i][25] = 5;
-            }else if (results[i][25] == "5"){
+            }else if (results[i][25] == "251-300"){
                 numValue[i][25] = 6;
-            }else if (results[i][25] == "6"){
+            }else if (results[i][25] == "301+"){
                 numValue[i][25] = 7;
             }
 
@@ -367,28 +342,44 @@ public class Calculate {
                 numValue[i][26] = 3;
             }else if (results[i][26] == "3"){
                 numValue[i][26] = 4;
-            }else if (results[i][26] == "4+"){
+            }else if (results[i][26] == "4"){
                 numValue[i][26] = 5;
+            }else if (results[i][26] == "5"){
+                numValue[i][26] = 6;
+            }else if (results[i][26] == "6"){
+                numValue[i][26] = 7;
             }
 
-            if (results[i][27] == "0-2"){
+            if (results[i][27] == "0"){
                 numValue[i][27] = 1;
-            }else if (results[i][27] == "3"){
+            }else if (results[i][27] == "1"){
                 numValue[i][27] = 2;
-            }else if (results[i][27] == "4"){
+            }else if (results[i][27] == "2"){
                 numValue[i][27] = 3;
-            }else if (results[i][27] == "5"){
+            }else if (results[i][27] == "3"){
                 numValue[i][27] = 4;
-            }else if (results[i][27] == "6"){
+            }else if (results[i][27] == "4+"){
                 numValue[i][27] = 5;
-            }else if (results[i][27] == "7"){
-                numValue[i][27] = 6;
             }
 
-            if (results[i][28] == "No"){
+            if (results[i][28] == "0-2"){
                 numValue[i][28] = 1;
-            }else if (results[i][28] == "Yes") {
+            }else if (results[i][28] == "3"){
                 numValue[i][28] = 2;
+            }else if (results[i][28] == "4"){
+                numValue[i][28] = 3;
+            }else if (results[i][28] == "5"){
+                numValue[i][28] = 4;
+            }else if (results[i][28] == "6"){
+                numValue[i][28] = 5;
+            }else if (results[i][28] == "7"){
+                numValue[i][28] = 6;
+            }
+
+            if (results[i][29] == "No"){
+                numValue[i][29] = 1;
+            }else if (results[i][29] == "Yes") {
+                numValue[i][29] = 2;
             }
 
             int waterPerWeek = Integer.parseInt(results[i][29]);
@@ -409,27 +400,8 @@ public class Calculate {
             }else if (waterPerWeek > 4000){
                 numValue[i][29] = 7;
             }
-            
+
         }
-
-        for (int i = 0; i < rsSize ; i++) {
-            switch (results[i][3]){
-
-            }
-        }
-
         return numValue;
     }
-
-
-
-
-
-
-
-
-
-
-
-
 }
